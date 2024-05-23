8d7ad856164a9b9e16c4fb8ffad0ccce0e190d4f
carlop13 <carlosguadalupelt13@gmail.com>
2024-05-22 21:31:41 -0600

    feat:(front): Config antd

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {Col,Row} from "antd";
+import './Layout.css';
+
+const LayoutComponent = () => {
+    return (
+        <div className="layout-container">
+        <Row>
+        <Col xs={0} sm={0} md={4} lg={6}>
+        <div className="content-left">
+        <h1 className="title">Izquierda</h1>
+        </div>
+        </Col>
+        <Col xs={24} sm={24} md={20} lg={18}>
+        <div className="content-rigth">
+        <h1 className="title">Derecha</h1>
+        </div>
+        </Col>
+        </Row>
+        {/*
+        <Row>
+        <Col xs={24} sm={24} md={20} lg={18}>
+        <div className="content-left">
+        <h1 className="title">Derecha</h1>
+        </div>
+        </Col>
+        </Row>
+    */}
+        </div>
+    );
+}
+
+export default LayoutComponent;
