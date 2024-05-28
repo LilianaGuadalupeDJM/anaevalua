a47cada9e59cc0ab861553ae53a1368d3ead0493
carlop13 <carlosguadalupelt13@gmail.com>
2024-05-27 20:27:43 -0600

    feat(front) Cambios en el componente layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {DatePicker, ConfigProvider, Button} from 'antd';
 import './App.css'
 import LayoutComponent from './components/Layout';
+import ImagenLogin from '.components/ImageLogin';
+import FormLogin from '.components/FormLogin';
+
 
 function App() {
 
@@ -12,7 +15,12 @@ function App() {
     }
    }}
    >
-    <LayoutComponent />
+    <LayoutComponent
+    leftColSize={{xs:0, sm:0, md:8, lg:6}}
+    rightColSize={{xs:24, sm:24, md:16, lg:18}}
+    leftContent={<ImagenLogin />}
+    rightContent={<FormLogin />}
+    />
    </ConfigProvider>
   )
 }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,30 +2,22 @@ import React from "react";
 import {Col,Row} from "antd";
 import './Layout.css';
 
-const LayoutComponent = () => {
+const LayoutComponent = ({leftColSize, rightColSize, leftContent, rightContent}) => {
     return (
         <div className="layout-container">
         <Row>
-        <Col xs={0} sm={0} md={4} lg={6}>
+        <Col xs={leftColSize.xs} sm={leftColSize.sm} md={leftColSize.md} lg={leftColSize.lg}>
         <div className="content-left">
-        <h1 className="title">Izquierda</h1>
+        {leftContent}
         </div>
+
         </Col>
-        <Col xs={24} sm={24} md={20} lg={18}>
+        <Col xs={rightColSize.xs} sm={rightColSize.sm} md={rightColSize.md} lg={rightColSize.lg}>
         <div className="content-rigth">
-        <h1 className="title">Derecha</h1>
+        {rightContent}
         </div>
         </Col>
         </Row>
-        {/*
-        <Row>
-        <Col xs={24} sm={24} md={20} lg={18}>
-        <div className="content-left">
-        <h1 className="title">Derecha</h1>
-        </div>
-        </Col>
-        </Row>
-    */}
         </div>
     );
 }
