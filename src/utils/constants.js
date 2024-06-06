1c1f6c24ac2e54d3bbaddc3498d1abdd3e04d151
carlop13 <carlosguadalupelt13@gmail.com>
2024-06-06 17:19:05 -0600

    correccion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,10 @@
 export const ENV = {
+    API_URL: 'http://localhost:3000',
+    ENDPOINTS: {
+        LOGIN: 'api/auth/sigin',
+        REGISTER: 'api/auth/signup',
+        USER: '/api/users',
+    },
     STORAGE: {
         TOKEN:"token",
     }
